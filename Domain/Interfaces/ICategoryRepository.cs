0354b17691d3fbe46e8a806f9a847b16f6610538
Gustavo Correia <gustavocorreiadias@gmail.com>
2025-03-16 21:18:42 -0300

    create repositories

---
+++ Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetByIdAsync(Guid id);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> CreateAsync(Product product);
+        Task<IEnumerable<Product>> GetAllCategoriesAsync();
+        Task<Product> GetByIdAsync(Guid id);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
