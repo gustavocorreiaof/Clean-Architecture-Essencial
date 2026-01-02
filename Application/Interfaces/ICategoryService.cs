2438c781e53b8e2d2a618c61f404b1fd125651e1
Gustavo Correia <gustavocorreiadias@gmail.com>
2026-01-02 19:54:55 -0300

    create category service

---
+++ Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+
+namespace Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync();
+        Task<CategoryDTO> GetByIdAsync(Guid id);
+        Task CreateCategoryAsync(CategoryDTO categoryDto);
+        Task UpdateCategoryAsync(CategoryDTO categoryDto);
+        Task RemoveCategoryAsync(Guid id);
+    }
+}
---
+++ Application/Services/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CategoryDTO categoryDto)
+        {
+            Category entity = _mapper.Map<Category>(categoryDto);
+            await _repository.CreateAsync(entity);
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetAllCategoriesAsync()
+        {
+            IEnumerable<Category> entities =  await _repository.GetAllCategoriesAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(entities);
+        }
+
+        public async Task<CategoryDTO> GetByIdAsync(Guid id)
+        {
+            Category entity  = await _repository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(entity);
+        }
+
+        public async Task RemoveCategoryAsync(Guid id)
+        {
+            Category entity = _repository.GetByIdAsync(id).Result;
+            await _repository.RemoveAsync(entity);
+        }
+
+        public async Task UpdateCategoryAsync(CategoryDTO categoryDto)
+        {
+            Category entity = _mapper.Map<Category>(categoryDto);
+            await _repository.UpdateAsync(entity);
+        }
+    }
+}
