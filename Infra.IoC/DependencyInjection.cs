452c5231f26aaa32efc085fbb62086f029d065a4
Gustavo Correia <gustavocorreiadias@gmail.com>
2026-01-03 20:57:42 -0300

    Add Categories management feature and update app entry point

--- Infra.IoC/DependencyInjection.cs
+++ Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Domain.Interfaces;
+﻿using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
 using Infra.Data.Context;
 using Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +20,12 @@ namespace Infra.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(cfg => {
+                cfg.AddProfile<DomainToDTOMappingProfile>();
+            });
 
             return services;
         }
