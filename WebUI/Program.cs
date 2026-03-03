a8718056873f2e70407822b8035df74d048f1a00
Gustavo Correia <gustavocorreiadias@gmail.com>
2026-03-03 12:04:15 -0300

    configure DI

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,7 +1,11 @@
+using Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddInfraestructure(builder.Configuration);
 
 var app = builder.Build();
 
