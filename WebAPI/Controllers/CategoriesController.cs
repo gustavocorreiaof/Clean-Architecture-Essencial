2f40865d9841d3d7528e12d528f032be46977640
Gustavo Correia <gustavocorreiadias@gmail.com>
2026-03-14 11:17:58 -0300

    create products and categories controller

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,78 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAll()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+
+            if (categories == null)
+                return NotFound("No categories found");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:guid}", Name = "GetById")]
+        public async Task<ActionResult<CategoryDTO>> GetById(Guid id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            if (category == null)
+                return BadRequest("There is no category with given Id.");
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+                return BadRequest("Category data is null.");
+
+            await _categoryService.CreateCategoryAsync(categoryDTO);
+
+            return new CreatedAtRouteResult("GetById", new { Id = categoryDTO.Id }, categoryDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Guid id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.Id)
+                return BadRequest("Id in the URL does not match Id in the body.");
+
+            if (categoryDTO == null)
+                return BadRequest();
+
+            await _categoryService.UpdateCategoryAsync(categoryDTO);
+
+            return Ok(categoryDTO);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<CategoryDTO>> Delete(Guid id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            
+            if (category == null)
+                return BadRequest("There is no category with given Id.");
+            
+            await _categoryService.RemoveCategoryAsync(id);
+            
+            return Ok(category);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var produtos = await _productService.GetAllProductsAsync();
+            if (produtos == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(Guid id)
+        {
+            var produto = await _productService.GetByIdAsync(id);
+            if (produto == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO produtoDto)
+        {
+            if (produtoDto == null)
+                return BadRequest("Data Invalid");
+
+            await _productService.CreateProductAsync(produtoDto);
+
+            return new CreatedAtRouteResult("GetProduct",
+                new { id = produtoDto.Id }, produtoDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(Guid id, [FromBody] ProductDTO produtoDto)
+        {
+            if (id != produtoDto.Id)
+            {
+                return BadRequest("Data invalid");
+            }
+
+            if (produtoDto == null)
+                return BadRequest("Data invalid");
+
+            await _productService.UpdateProductAsync(produtoDto);
+
+            return Ok(produtoDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(Guid id)
+        {
+            var produtoDto = await _productService.GetByIdAsync(id);
+
+            if (produtoDto == null)
+            {
+                return NotFound("Product not found");
+            }
+
+            await _productService.RemoveProductAsync(id);
+
+            return Ok(produtoDto);
+        }
+    }
+}
